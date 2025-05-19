b288e0f61f346195fba67160a0c3bab07588a8de
andruhes <116680804+andruhes@users.noreply.github.com>
2025-05-19 15:18:38 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import smtplib
+from email.message import EmailMessage
+
+
+# Почтовые настройки
+TO = 'it@andruhes.ru'
+FROM = 'napominalka@andruhes.ru'
+SUBJECT = 'Напоминание по работе сервера'
+BODY = 'Выгрузить из РП ЗУП файл и отправить на почту мне'
+SMTP_SERVER = '192.168.100.5'   # Адрес вашего SMTP-сервера
+# SMTP_PORT = 587                 # Порт SMTP-сервера (обычно 587 для STARTTLS)
+# USERNAME = 'ВашЛогин'           # Логин пользователя для входа на почтовый сервер
+# PASSWORD = 'ВашПароль'          # Пароль пользователя для входа на почтовый сервер
+
+# Создаем экземпляр электронного письма
+msg = EmailMessage()
+msg['Subject'] = SUBJECT
+msg['From'] = FROM
+msg['To'] = TO
+msg.set_content(BODY)
+
+try:
+    # Подключаемся к SMTP-серверу и отправляем письмо
+    with smtplib.SMTP(SMTP_SERVER) as server:
+#   with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+     #    server.starttls()              # Включаем шифрованное соединение TLS
+     #    server.login(USERNAME, PASSWORD)  # Авторизуемся на сервере
+        server.send_message(msg)
+    print("Email успешно отправлен!")
+except Exception as e:
+    print(f"Произошла ошибка при отправке письма: {e}")
+
+
+
+import smtplib
+from email.message import EmailMessage
+
